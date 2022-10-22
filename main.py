0b5a3e896e88d2a9a98b468bc16e0e1c000351fd
Samburov Pavel <samburov01@bk.ru>
2022-10-22 18:57:46 +0300

    version_3

--- main.py
+++ main.py
@@ -6,3 +6,22 @@ def sum_of_numbers(*args):
         result += arg
 
     return result
+
+
+def str_is_palindrome(initial_string):
+    index = 0
+    for reverse_index in range(len(initial_string), 0, -1):
+        reverse_index -= 1
+        if initial_string[reverse_index] != initial_string[index]:
+            return False
+        index += 1
+    return True
+
+
+def split_str_space(initial_str):
+    result = ""
+    split_str = initial_str.split()
+    for i in split_str:
+        result += i
+
+    return result
